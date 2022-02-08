381044fd40299ea801767fbd996994c97dc5b14f
Alexander Skiba <ghostlyrics@gmail.com>
2022-02-08 01:56:12 +0100

    Move blacklist and cached mapping to caches

--- utils/caches.py
+++ utils/caches.py
@@ -1,12 +1,14 @@
 """Implementations of caches."""
 
 # default imports
+import csv
 import datetime
 import json
 import hashlib
 import logging
+import operator
 import os
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, List
 
 # 3rd party imports
 from appdirs import user_cache_dir
@@ -14,7 +16,84 @@ from dateutil.parser import parse
 from .utils import format_log
 
 
-class JikanResultCache:
+class Blacklist:
+    """Implement a list of items that cannot be mapped from anime-planet.com to myanimelist.net."""
+
+    cache = []  # type: List[List[str]]
+    cache_file = ""
+
+    def __init__(self, blacklist_file: str) -> None:
+        """Create a new Blacklist."""
+        self.cache_file = blacklist_file
+
+    def __enter__(self) -> "Blacklist":
+        """Context manager for Blacklist to simplify cache load."""
+        with open(self.cache_file, encoding='utf-8', mode='r') as cache:
+            reader = csv.reader(cache)
+            self.cache = list(reader)
+        return self
+
+    def __exit__(self, exception_type: Any, exception_value: Any, traceback: Any) -> None:
+        """Context manager for Blacklist to simplify cache save."""
+        with open(self.cache_file, encoding='utf-8', mode='w') as cache:
+            writer = csv.writer(cache, quoting=csv.QUOTE_ALL)
+            writer.writerows(sorted(self.cache))
+
+    def add(self, name: str) -> None:
+        """Add a title to the blacklist."""
+
+    def lookup(self, name: str) -> bool:
+        """Check if a title is blacklisted."""
+        for title in self.cache:
+            entry = format_log({"name": name})
+
+            if title[0] == name:
+                logging.info("Blacklist found: %s", entry)
+                return True
+        return False
+
+
+class MappingCache:
+    """Implement a mapping of anime-planet.com anime title to myanimelist.net ID."""
+
+    def __init__(self, cache_file: str) -> None:
+        """Create a new MappingCache."""
+        self.cache_file = cache_file
+
+    def add(self, name: str, myanimelist_id: int) -> None:
+        """Add a mapping pair to the cache."""
+        with open(self.cache_file, "r", encoding="utf-8") as before:
+            cache = csv.reader(before)
+            content = list(cache)
+            content.append([name, str(myanimelist_id)])
+            content = sorted(content, key=operator.itemgetter(0))
+
+        with open(self.cache_file, "w", encoding="utf-8") as after:
+            writer = csv.writer(after, quoting=csv.QUOTE_ALL)
+            writer.writerows(content)
+
+        entry = format_log({"name": name, "myanimelist_id": myanimelist_id})
+        logging.info("MappingCache added: %s", entry)
+
+    def lookup(self, name: str) -> Optional[str]:
+        """Look for a title in the cache."""
+        with open(self.cache_file, "r", encoding="utf-8") as cache:
+            reader = csv.reader(cache)
+            data = list(reader)
+
+        mapping = {}  # type: Dict[str, str]
+        for row in data:
+            mapping.update({row[0]: row[1]})
+
+        if name in mapping:
+            myanimelist_id = mapping[name]
+            entry = format_log({"name": name, "myanimelist_id": myanimelist_id})
+            logging.info("MappingCache found: %s", entry)
+            return myanimelist_id
+        return None
+
+
+class RequestCache:
     """Implement a hash-addressed cache for Jikan results.
 
     The cache uses hashes instead of strings because anime like
@@ -27,7 +106,7 @@ class JikanResultCache:
     _supported_types = ["anime_title", "anime_search"]
 
     def __init__(self) -> None:
-        """Create a new JikanResultCache."""
+        """Create a new RequestCache."""
         self.location = os.path.join(user_cache_dir(), "anitransfer", "jikan_cache")
 
     def add(self, name: str, item_type: str, item: Dict[str, Any]) -> None:
@@ -42,7 +121,7 @@ class JikanResultCache:
             json.dump(item, cache_entry, indent=2, sort_keys=False)
 
         entry = format_log({"name": name, "item_type": item_type, "key": key})
-        logging.info("Added to cache: %s", entry)
+        logging.info("JikanRequestCache: added: %s", entry)
 
     def lookup(self, name: str, item_type: str) -> Optional[Dict[str, Any]]:
         """Look for an unexpired entry in the cache."""
@@ -52,9 +131,9 @@ class JikanResultCache:
         location = os.path.join(cache_location, f"{key}.json")
 
         try:
-            with open(location, 'r', encoding='utf8') as cache_entry:
+            with open(location, "r", encoding="utf8") as cache_entry:
                 entry = json.load(cache_entry)  # type: Dict[str, Any]
-                expiry_date = entry['headers']['Expires']
+                expiry_date = entry["headers"]["Expires"]
         # FileNotFoundError: there is no such cache entry
         # KeyError: there is no expiration date in the entry
         # both of those will cause a cache miss
@@ -66,15 +145,15 @@ class JikanResultCache:
         if now > date:
             return None
 
-        message = format_log({'name': name, 'item_type': item_type, 'key': key})
-        logging.info('Jikan Cache hit: %s', message)
+        message = format_log({"name": name, "item_type": item_type, "key": key})
+        logging.info("RequestCache: found: %s", message)
         return entry
 
     def _verify_type_is_supported(self, item_type: str) -> None:
         """Ensure the used cache is implemented."""
         if item_type not in self._supported_types:
             raise ValueError(
-                f"JikanResultCache does not implement cache of type {item_type}."
+                f"RequestCache does not implement cache of type {item_type}."
             )
 
 
