2aaebbb4d19b5e8df221b08f2ac094107ef77309
Alexander Skiba <ghostlyrics@gmail.com>
2022-02-08 03:41:22 +0100

    Resolve linter warnings for exporters

--- utils/exporters.py
+++ utils/exporters.py
@@ -5,7 +5,6 @@ import xml.etree.cElementTree as ET
 from typing import Any, List, Optional, Dict, Union
 
 # 3rd party imports
-from rich import print
 from defusedxml import minidom
 
 # internal imports
@@ -17,7 +16,7 @@ MAPPING = {
     "want to watch": "Plan to Watch",
     "stalled": "On-Hold",
     "dropped": "Dropped",
-    "won't watch": None
+    "won't watch": None,
 }
 
 
@@ -29,6 +28,7 @@ class AnimeExporter:
     cache = None  # type: MappingCache
 
     def __init__(self, username: str, cache_file: str) -> None:
+        """Create a new AnimeExporter."""
         self.cache = MappingCache(cache_file)
         self.username = username
 
@@ -38,65 +38,101 @@ class AnimeExporter:
         Returns False if an unsupported status was filtered,
         True otherwise.
         """
-        if MAPPING[anime['status']]:
+        if MAPPING[anime["status"]]:
             self.anime_to_export.append(anime)
             return True
         return False
 
-    def export(self, filepath:str) -> None:
+    def export(self, filepath: str) -> None:
         """Write the export to a file."""
-        root = ET.Element('myanimelist')
-        info = ET.SubElement(root, 'myinfo')
+        root = ET.Element("myanimelist")
+        info = ET.SubElement(root, "myinfo")
 
         # username
-        username = ET.SubElement(info, 'user_name')
+        username = ET.SubElement(info, "user_name")
         username.text = self.username
 
         # total anime watched
-        total = ET.SubElement(info, 'user_total_anime')
+        total = ET.SubElement(info, "user_total_anime")
         total.text = str(len(self.anime_to_export))
 
         # anime section
         for anime in self.anime_to_export:
-            entry = ET.SubElement(root, 'anime')
+            entry = ET.SubElement(root, "anime")
 
-            myanimelist_id = ET.SubElement(entry, 'series_animedb_id')
-            myanimelist_id.text = self.cache.lookup(str(anime['name']))
+            # ID
+            ET.SubElement(entry, "series_animedb_id").text = self.cache.lookup(
+                str(anime["name"])
+            )
 
-            title = ET.SubElement(entry, 'series_title')
-            title.text = str(anime['name'])
+            # title
+            ET.SubElement(entry, "series_title").text = str(anime["name"])
 
-            watched = ET.SubElement(entry, 'my_watched_episodes')
-            watched.text = str(anime['eps'])
+            # progress
+            ET.SubElement(entry, "my_watched_episodes").text = str(anime["eps"])
 
-            started = ET.SubElement(entry, 'my_start_date')
-            if not anime['started']:
-                started.text = "0000-00-00"
-            else:
-                started.text = str(anime['started']).split()[0]
+            # start date
+            ET.SubElement(entry, "my_start_date").text = _fill_date(
+                anime.get("started")
+            )
 
-            finished = ET.SubElement(entry, 'my_finish_date')
-            if not anime['completed']:
-                finished.text = "0000-00-00"
-            else:
-                finished.text = str(anime['completed']).split()[0]
+            # completed date
+            ET.SubElement(entry, "my_finish_date").text = _fill_date(
+                anime.get("completed")
+            )
 
-            score = ET.SubElement(entry, 'my_score ')
+            # score
             # anime-planet.com defaults to 0.5-5 ratings, in steps of 0.5
             # myanimelist.net defaults to ratings of 1-10, in steps of 1
-            score.text = str(int(anime['rating'] * 2))
+            score = ET.SubElement(entry, "my_score ")
+            if anime["rating"]:
+                score.text = str(int(anime["rating"] * 2))
 
-            status = ET.SubElement(entry, 'my_status')
-            status.text = MAPPING[str(anime['status'])]
+            # status
+            ET.SubElement(entry, "my_status").text = MAPPING[str(anime["status"])]
 
-            times_watched = ET.SubElement(entry, 'my_times_watched')
-            times = 0
-            # logic taken as is from original
-            if int(anime['times']) > 1:
-                times = int(anime['times']) - 1
-            times_watched.text = str(times)
+            # amount of times watched
+            ET.SubElement(entry, "my_times_watched").text = _convert_times_watched(
+                anime.get("times")
+            )
 
         dom = minidom.parseString(ET.tostring(root))
-        pretty_dom = dom.toprettyxml(indent='\t')
-        with open(filepath, 'w', encoding='utf-8') as export_file:
+        pretty_dom = dom.toprettyxml(indent="\t")
+        with open(filepath, "w", encoding="utf-8") as export_file:
             export_file.write(pretty_dom)
+
+
+def _fill_date(date: Optional[Union[int, str]]) -> str:
+    """Convert different datetime stamps between sites.
+
+    Provide a default of 0000-00-00 if no date is found.
+    """
+    if not date or not isinstance(date, str):
+        converted_date = "0000-00-00"
+    else:
+        # remove time and leave only date
+        converted_date = date.split()[0]
+
+    return converted_date
+
+
+def _convert_times_watched(times: Optional[Union[int, str]]) -> str:
+    """Convert different counting systems between sites.
+
+    Provide a default of 0 if no amount is found.
+    """
+    # case: times is not assigned
+    if not times:
+        times = 0
+
+    # case: times is a string
+    if isinstance(times, str):
+        if times.isdigit():
+            times = int(times)
+        else:
+            times = 0
+
+    if times > 1:
+        times = times - 1
+
+    return str(times)
