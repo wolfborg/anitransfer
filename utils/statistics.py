8e41f2a582e972f5024a53e30c34cf624b965a78
Alexander Skiba <ghostlyrics@gmail.com>
2022-02-08 01:55:27 +0100

    Implement statistics as class

---
+++ utils/statistics.py
@@ -0,0 +1,79 @@
+"""Implementation of statistics."""
+
+# default imports
+from typing import Dict
+
+# 3rd party imports
+from rich.console import Console
+from rich.table import Table
+
+
+class Statistics:
+    """Hold statistics about the current anitransfer run."""
+
+    counters = {}  # type: Dict[str, int]
+
+    def __init__(self) -> None:
+        """Create a new Statistics instance."""
+        self.counters = {
+            "entries_processed": 0,
+            "entries_matched_manually": 0,
+            "entries_matched_using_jikan": 0,
+            "entries_matched_using_cache": 0,
+            "entries_matched_using_blacklist": 0,
+            "entries_unmatched": 0,
+            "entries_unsupported": 0,
+            "jikan_requests_total": 0,
+            "jikan_requests_cached": 0,
+            "jikan_requests_failed": 0,
+        }
+
+    def increment(self, name: str, amount: int = 1) -> None:
+        """Increment value for a given statistic name."""
+        self.counters.update({name: self.counters.get(name, 0) + amount})
+
+    def print_summary(self) -> None:
+        """Print a formatted summary of the statistics."""
+        table = Table(title="anitransfer summary")
+
+        table.add_column("Section")
+        table.add_column("Action")
+        table.add_column("Count", justify="right")
+
+        table.add_row("Entries", "processed", str(self.counters["entries_processed"]))
+        table.add_row("Entries", "unmatched", str(self.counters["entries_unmatched"]))
+        table.add_row("Entries", "dropped due to unsupported status", str(self.counters["entries_unsupported"]))
+        table.add_row(
+            "Matches",
+            "matched via Jikan",
+            str(self.counters["entries_matched_using_jikan"]),
+        )
+        table.add_row(
+            "Matches",
+            "matched via cache",
+            str(self.counters["entries_matched_using_cache"]),
+        )
+        table.add_row(
+            "Matches",
+            "excluded via blacklist",
+            str(self.counters["entries_matched_using_blacklist"]),
+        )
+        table.add_row(
+            "Matches",
+            "matched manually",
+            str(self.counters["entries_matched_manually"]),
+        )
+        table.add_row(
+            "Requests", "requests made", str(self.counters["jikan_requests_total"])
+        )
+        table.add_row(
+            "Requests",
+            "requests skipped due to cache",
+            str(self.counters["jikan_requests_cached"]),
+        )
+        table.add_row(
+            "Requests", "requests failed", str(self.counters["jikan_requests_failed"])
+        )
+
+        console = Console()
+        console.print(table, new_line_start=True)
